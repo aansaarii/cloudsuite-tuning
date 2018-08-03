a356808e7be4055b2548f17674f77bc625548291
Rahul Sharma <rahul.kenu@gmail.com>
2018-08-03 12:16:27 +0200

    Updated scripts for data-serving

--- data-serving/benchmark_run.sh
+++ data-serving/benchmark_run.sh
@@ -3,7 +3,10 @@ if [ "$#" -ne 2 ]; then
     echo "usage: Command RECORDS OPERATIONS_FILE"
     exit 1
 fi
-
+THREADS=42
+TARGET=100000
+CLIENT_CPUS=0-5
+SERVER_CPUS=6-7
 CLIENT_CONTAINER=cassandra-client
 SERVER_CONTAINER=cassandra-server
 RECORDS=$1
@@ -25,7 +28,7 @@ docker network rm serving_network
 docker system prune -f
 
 docker network create serving_network
-docker run --rm -d --name $SERVER_CONTAINER --cpuset-cpus="4-7" --net serving_network server
+docker run --rm -d --name $SERVER_CONTAINER --cpuset-cpus=$SERVER_CPUS --net serving_network server
 
 while [ 1 ]; do
     docker logs $SERVER_CONTAINER | grep 'Created default superuser role' &> /dev/null
@@ -39,7 +42,7 @@ while [ 1 ]; do
 done
 
 
-docker run --rm -it -d --cpuset-cpus="0-3" --name $CLIENT_CONTAINER --net serving_network client "cassandra-server"
+docker run --rm -it -d --cpuset-cpus=$CLIENT_CPUS --name $CLIENT_CONTAINER --net serving_network client "cassandra-server"
 
 
 
@@ -56,13 +59,15 @@ done
 
 
 docker exec -it CLIENT_CONTAINER "/ycsb/bin/ycsb load cassandra-cql -p hosts=$SERVER_CONTAINER -P /ycsb/workloads/workloada -p recordcount=$RECORDS"
-
-
+mpstat -P 0-7 1 &
+echo "WARMING UP"
+pkill mpstat
+docker exec $CLIENT_CONTAINER bash -c "/ycsb/bin/ycsb run cassandra-cql -p hosts=$SERVER_CONTAINER -P /ycsb/workloads/workloada -s -target $TARGET -threads $THREADS -p operationcount=10000"
 while read OPERATIONS; do
     echo "NUM OPERATIONS = $OPERATIONS"
     echo $OPERATIONS>>$OPERATIONSFILE
     echo "@">>$UTILFILE
     mpstat -P 0-7 1 >> $UTILFILE &
-    (docker exec $CLIENT_CONTAINER bash -c "/ycsb/bin/ycsb run cassandra-cql -p hosts=$SERVER_CONTAINER -P /ycsb/workloads/workloada -p operationcount=$OPERATIONS")>>$BENCHMARKFILE
+    (docker exec $CLIENT_CONTAINER bash -c "/ycsb/bin/ycsb run cassandra-cql -p hosts=$SERVER_CONTAINER -P /ycsb/workloads/workloada -s -target $TARGET -threads $THREADS -p operationcount=$OPERATIONS")>>$BENCHMARKFILE
     pkill mpstat
 done < $OPERATIONS_FILE
