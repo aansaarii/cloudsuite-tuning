38c1a215b64503a031f64b1158b3b53ed926eaeb
Rahul Sharma <rahul.kenu@gmail.com>
2018-08-06 15:57:54 +0200

    updateS

--- data-serving/benchmark_run.sh
+++ data-serving/benchmark_run.sh
@@ -3,32 +3,42 @@ if [ "$#" -ne 2 ]; then
     echo "usage: Command RECORDS OPERATIONS_FILE"
     exit 1
 fi
-THREADS=42
+THREADS=10
+THREADS_LOAD=10
 TARGET=100000
-CLIENT_CPUS=0-5
-SERVER_CPUS=6-7
+CLIENT_CPUS=0-3
+SERVER_CPUS=4-7
+SERVER_MEMORY=10g
 CLIENT_CONTAINER=cassandra-client
 SERVER_CONTAINER=cassandra-server
 RECORDS=$1
 OPERATIONS_FILE=$2
-
+LOAD=true
 OUTPUTFOLDER=output
 UTILFILE=$OUTPUTFOLDER/util.txt
 OPERATIONSFILE=$OUTPUTFOLDER/operations.txt
 DISPLAYFILE=$OUTPUTFOLDER/display.txt
 BENCHMARKFILE=$OUTPUTFOLDER/benchmark.txt
-
+BENCHMARKFILE2=$OUTPUTFOLDER/benchmark2.txt
 rm $UTILFILE && touch $UTILFILE
 rm $OPERATIONSFILE && touch $OPERATIONSFILE
 rm $BENCHMARKFILE && touch $BENCHMARKFILE
-
+rm $BENCHMARKFILE2 && touch $BENCHMARKFILE2
 docker rm -f $CLIENT_CONTAINER
-docker rm -f $SERVER_CONTAINER
+if [ "$LOAD" = true ]
+then    
+    docker rm -f $SERVER_CONTAINER
+fi
+
 docker network rm serving_network
 docker system prune -f
 
 docker network create serving_network
-docker run --rm -d --name $SERVER_CONTAINER --cpuset-cpus=$SERVER_CPUS --net serving_network server
+if [ "$LOAD" = true ]
+then
+    docker run -d --name $SERVER_CONTAINER --cpuset-cpus=$SERVER_CPUS --net serving_network --memory=$SERVER_MEMORY server
+fi
+
 
 while [ 1 ]; do
     docker logs $SERVER_CONTAINER | grep 'Created default superuser role' &> /dev/null
@@ -40,7 +50,6 @@ while [ 1 ]; do
     echo "Sleeping"
     sleep 5
 done
-
 
 docker run --rm -it -d --cpuset-cpus=$CLIENT_CPUS --name $CLIENT_CONTAINER --net serving_network client "cassandra-server"
 
@@ -56,18 +65,75 @@ while [ 1 ]; do
     echo "Sleeping"
     sleep 5
 done
+echo $RECORDS
+echo $SERVER_CONTAINER
+
+#read -p "BEFORE LOAD"
 
 
-docker exec -it CLIENT_CONTAINER "/ycsb/bin/ycsb load cassandra-cql -p hosts=$SERVER_CONTAINER -P /ycsb/workloads/workloada -p recordcount=$RECORDS"
+while [ 1 ]; do
+    docker logs $SERVER_CONTAINER | grep 'Created default superuser role' &> /dev/null
+    if [ $? == 0 ]; then
+	echo "Done"
+	break;
+    fi
+    docker logs $SERVER_CONTAINER
+    echo "Sleeping"
+    sleep 5
+done
+
+docker run --rm -it -d --cpuset-cpus=$CLIENT_CPUS --name $CLIENT_CONTAINER --net serving_network client "cassandra-server"
+
+
+
+while [ 1 ]; do
+    docker logs $CLIENT_CONTAINER | grep 'Keyspace usertable was created' &> /dev/null
+    if [ $? == 0 ]; then
+	echo "Done"
+	break;
+    fi
+    docker logs $CLIENT_CONTAINER
+    echo "Sleeping"
+    sleep 5
+done
+echo $RECORDS
+echo $SERVER_CONTAINER
+
+#read -p "BEFORE LOAD"
+if [ "$LOAD" = true ]
+then
+    docker exec -it $CLIENT_CONTAINER bash -c "/ycsb/bin/ycsb load cassandra-cql -p hosts=$SERVER_CONTAINER -P /ycsb/workloads/workloada -s -target $TARGET -threads $THREADS_LOAD -p recordcount=$RECORDS"
+fi
+
+#read -p "AFTER LOAD"
 mpstat -P 0-7 1 &
 echo "WARMING UP"
-pkill mpstat
+
 docker exec $CLIENT_CONTAINER bash -c "/ycsb/bin/ycsb run cassandra-cql -p hosts=$SERVER_CONTAINER -P /ycsb/workloads/workloada -s -target $TARGET -threads $THREADS -p operationcount=10000"
+pkill mpstat
+
 while read OPERATIONS; do
     echo "NUM OPERATIONS = $OPERATIONS"
     echo $OPERATIONS>>$OPERATIONSFILE
     echo "@">>$UTILFILE
+   # docker rm -f $CLIENT_CONTAINER
+   # docker run --rm -it -d --cpuset-cpus=$CLIENT_CPUS --name $CLIENT_CONTAINER --net serving_network client "cassandra-server"
+    # while [ 1 ]; do
+    # 	docker logs $CLIENT_CONTAINER | grep 'Keyspace usertable was created' &> /dev/null
+    # 	if [ $? == 0 ]; then
+    # 	    echo "Done"
+    # 	    break;
+    # 	fi
+    # 	docker logs $CLIENT_CONTAINER
+    # 	echo "Sleeping"
+    # 	sleep 5
+    # done
+    docker exec $CLIENT_CONTAINER bash -c "/ycsb/bin/ycsb run cassandra-cql -p hosts=$SERVER_CONTAINER -P /ycsb/workloads/workloada -s -target $TARGET -threads $THREADS -p operationcount=10000"
     mpstat -P 0-7 1 >> $UTILFILE &
-    (docker exec $CLIENT_CONTAINER bash -c "/ycsb/bin/ycsb run cassandra-cql -p hosts=$SERVER_CONTAINER -P /ycsb/workloads/workloada -s -target $TARGET -threads $THREADS -p operationcount=$OPERATIONS")>>$BENCHMARKFILE
+    (docker exec $CLIENT_CONTAINER bash -c "/ycsb/bin/ycsb run cassandra-cql -p hosts=$SERVER_CONTAINER -P /ycsb/workloads/workloada -s -target $TARGET -threads $THREADS -p operationcount=$OPERATIONS")>>$BENCHMARKFILE &
+    pid1=$!
+#    (docker exec $CLIENT_CONTAINER bash -c "/ycsb/bin/ycsb run cassandra-cql -p hosts=$SERVER_CONTAINER -P /ycsb/workloads/workloada -s -target $TARGET -threads $THREADS -p operationcount=$OPERATIONS")>>$BENCHMARKFILE2 &
+ #   pid2=$!
+    wait $pid1 $pid2
     pkill mpstat
 done < $OPERATIONS_FILE
