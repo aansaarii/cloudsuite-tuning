346aa1bfee7d58cdbbe6053356127cbfcb1e756d
zilutian <zilu.tian@epfl.ch>
2019-06-01 12:06:30 -0400

    Update schedule

--- web-search/schedule.sh
+++ web-search/schedule.sh
@@ -1,19 +1,34 @@
 #!/bin/bash 
 
 LOGFILE=log 
-LOGFOLDER=cavium/multithreads
+LOGFOLDER=cavium/experimental2/
 
 echo "Run started" > $LOGFILE 
 date -u >> $LOGFILE
 ./benchmark_run.sh load.txt 
-mv output ${LOGFOLDER}/single-thread
+mv output ${LOGFOLDER}/0
 
 echo "Run completed" >> $LOGFILE
 date -u >> $LOGFILE
 
-MOD=( 's/CLIENT_CPUS=0/CLIENT_CPUS=0,1/g;s/SERVER_CPUS=28/SERVER_CPUS=28,29/g'\
-'s/CLIENT_CPUS=0,1/CLIENT_CPUS=0,1,2/g;s/SERVER_CPUS=28,29/SERVER_CPUS=28,29,30/g'\
-'s/CLIENT_CPUS=0,1,2/CLIENT_CPUS=0,1,2,3/g;s/SERVER_CPUS=28,29,30/SERVER_CPUS=28,29,30,31/g'
+if [ 0 -eq 1 ]; then 
+MOD=( 's/CLIENT_CPUS=.*$/CLIENT_CPUS=0-1/g;s/SERVER_CPUS=.*$/SERVER_CPUS=28-29/g' \
+'s/CLIENT_CPUS=.*$/CLIENT_CPUS=0-2/g;s/SERVER_CPUS=.*$/SERVER_CPUS=28-30/g' \
+'s/CLIENT_CPUS=.*$/CLIENT_CPUS=0-3/g;s/SERVER_CPUS=.*$/SERVER_CPUS=28-31/g' \
+'s/CLIENT_CPUS=.*$/CLIENT_CPUS=0-4/g;s/SERVER_CPUS=.*$/SERVER_CPUS=28-32/g' \
+'s/CLIENT_CPUS=.*$/CLIENT_CPUS=0-13/g;s/SERVER_CPUS=.*$/SERVER_CPUS=28-41/g' \
+'s/CLIENT_CPUS=.*$/CLIENT_CPUS=0-27/g;s/SERVER_CPUS=.*$/SERVER_CPUS=28-55/g' 
+)
+fi 
+
+sed -ri s/START_SERVER.*$/START_SERVER=false/g benchmark_run.sh
+
+MOD=( 's/SERVER_CPUS=.*$/SERVER_CPUS=28-29/g' \
+'s/SERVER_CPUS=.*$/SERVER_CPUS=28-30/g' \
+'s/SERVER_CPUS=.*$/SERVER_CPUS=28-31/g' \
+'s/SERVER_CPUS=.*$/SERVER_CPUS=28-32/g' \
+'s/SERVER_CPUS=.*$/SERVER_CPUS=28-41/g' \
+'s/SERVER_CPUS=.*$/SERVER_CPUS=28-55/g'
 )
 
 CNT=0
@@ -30,7 +45,6 @@ while [[ ${CNT} -le ${MAX} ]]; do
 	date -u >> $LOGFILE
 	
 	CNT=$((CNT + 1))
-    OUTCNT=$((CNT+1))	
-	mv output ${LOGFOLDER}/${OUTCNT}-thread
+	mv output ${LOGFOLDER}/${CNT}
 done
 
