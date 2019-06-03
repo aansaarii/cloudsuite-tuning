d440f7a6a6d364bfa972e028713d3a645fc7da1a
ZiluTian <zilu.tian@epfl.ch>
2019-06-03 17:17:47 -0400

    Vary client/server config

--- web-search/schedule.sh
+++ web-search/schedule.sh
@@ -1,65 +1,80 @@
 #!/bin/bash 
 
+# set -x 
 LOGFILE=log 
-LOGFOLDER=cavium/experimental6/
+LOGFOLDER=cavium/single-core-client/
+rm -rf /tmp/ws_scheduler.lock
+rm -rf server-finished
+rm -rf ${LOGFOLDER}/*
 
-echo "Run started" > $LOGFILE 
+cp benchmark_run.sh $LOGFOLDER/benchmark_cp 
+
+echo "Initial run started" > $LOGFILE 
 date -u >> $LOGFILE
-./benchmark_run.sh load.txt 
+
+./benchmark_run.sh load.txt
+#echo "hello" > output 
 mv output ${LOGFOLDER}/0
 
 echo "Run completed" >> $LOGFILE
 date -u >> $LOGFILE
 
-if [ 0 -eq 1 ]; then 
-MOD=( 's/CLIENT_CPUS=.*$/CLIENT_CPUS=0-1/g;s/SERVER_CPUS=.*$/SERVER_CPUS=28-29/g' \
-'s/CLIENT_CPUS=.*$/CLIENT_CPUS=0-2/g;s/SERVER_CPUS=.*$/SERVER_CPUS=28-30/g' \
-'s/CLIENT_CPUS=.*$/CLIENT_CPUS=0-3/g;s/SERVER_CPUS=.*$/SERVER_CPUS=28-31/g' \
-'s/CLIENT_CPUS=.*$/CLIENT_CPUS=0-4/g;s/SERVER_CPUS=.*$/SERVER_CPUS=28-32/g' \
-'s/CLIENT_CPUS=.*$/CLIENT_CPUS=0-13/g;s/SERVER_CPUS=.*$/SERVER_CPUS=28-41/g' \
-'s/CLIENT_CPUS=.*$/CLIENT_CPUS=0-27/g;s/SERVER_CPUS=.*$/SERVER_CPUS=28-55/g' 
-)
+function change_servers() {
+    MOD=( 's/SERVER_CPUS=.*$/SERVER_CPUS=112-117/g' \
+    's/SERVER_CPUS=.*$/SERVER_CPUS=112,140/g' \
+    's/SERVER_CPUS=.*$/SERVER_CPUS=112,140,168,196/g' \
+    's/SERVER_CPUS=.*$/SERVER_CPUS=112-117,140-145/g' \
+    's/SERVER_CPUS=.*$/SERVER_CPUS=112-117,140-145,168-173,196-201/g' \
+    's/SERVER_CPUS=.*$/SERVER_CPUS=112-167/g' \
+    's/SERVER_CPUS=.*$/SERVER_CPUS=112-223/g' 
+    )
 
-sed -ri s/^START_SERVER.*$/START_SERVER=false/g benchmark_run.sh
+    CNT=0
+    MAX=${#MOD[@]}
+    lockdir=/tmp/ws_scheduler.lock 
 
-MOD=( 's/SERVER_CPUS=.*$/SERVER_CPUS=112,140/g' \
-'s/SERVER_CPUS=.*$/SERVER_CPUS=112,140,168/g' \
-'s/SERVER_CPUS=.*$/SERVER_CPUS=112,140,168,196/g' \
-'s/SERVER_CPUS=.*$/SERVER_CPUS=112-113,140-141,168-169,196-197/g' \
-'s/SERVER_CPUS=.*$/SERVER_CPUS=112-115,140-143,168-171,196-199/g' \
-'s/SERVER_CPUS=.*$/SERVER_CPUS=112-125,140-153,168-181,196-209/g' \
-'s/SERVER_CPUS=.*$/SERVER_CPUS=112-223/g' 
-)
-fi 
-
-MOD=( 's/^CLIENT_CPUS=.*$/CLIENT_CPUS=0,1/g' \
-'s/^CLIENT_CPUS=.*$/CLIENT_CPUS=0,1,2/g' \
-'s/^CLIENT_CPUS=.*$/CLIENT_CPUS=0,1,2,3/g' \
-'s/^CLIENT_CPUS=.*$/CLIENT_CPUS=0,1,2,3,4/g' \
-'s/^CLIENT_CPUS=.*$/CLIENT_CPUS=0,1,2,3,4,5/g' \
-'s/^CLIENT_CPUS=.*$/CLIENT_CPUS=0,1,2,3,4,5,6/g' 
-)
-CNT=0
-MAX=${#MOD[@]}
-lockdir=/tmp/ws_scheduler.lock 
-
-while [[ ${CNT} -le ${MAX} ]]; do
-	# echo "Run ${CNT} started" >> $LOGFILE
-		
-	if mkdir $lockdir
-	then 
-	echo "Run ${CNT} started" >> $LOGFILE
-	FOO=MOD[${CNT}]
-	
-	sed -ri ${!FOO} cpu_assign.sh
-	./benchmark_run.sh load.txt
-
-	echo "Run completed" >> $LOGFILE
-	date -u >> $LOGFILE
-	
-	CNT=$((CNT + 1))
-	mv output ${LOGFOLDER}/${CNT}
-	rm $lockdir
+    while [[ ${CNT} -lt ${MAX} ]]; do
+	    if mkdir $lockdir
+	    then 
+	    echo "Run ${CNT} started" >> $LOGFILE
+	    FOO=MOD[${CNT}]
+	    
+	    sed -ri ${!FOO} cpu_assign.sh
+ 	    ./benchmark_run.sh load.txt
+	    # echo "${!FOO}" > output 
+	    echo "Run completed" >> $LOGFILE
+	    date -u >> $LOGFILE
+	    
+	    CNT=$((CNT + 1))
+	    mv output ${LOGFOLDER}/${CNT}
+	    rm -rf $lockdir
 	fi 
-done
+    done
+    
+    if [ ${CNT} -eq ${MAX} ]; then 
+	mkdir server-finished	
+    fi 
+}
 
+change_servers &
+wait $! 
+
+if [ -d server-finished ]; then  
+    rm -rf server-finished
+    sed -ri 's/CLIENT_CPUS=.*$/CLIENT_CPUS=0-5/g' cpu_assign.sh 
+    sed -ri 's/SERVER_CPUS=.*$/SERVER_CPUS=112/g' cpu_assign.sh
+    LOGFOLDER=cavium/single-chip-client/
+    rm -rf /tmp/ws_scheduler.lock
+    rm -rf ${LOGFOLDER}/*
+
+    date -u >> $LOGFILE
+    ./benchmark_run.sh load.txt 
+    # echo "Hello again" > output 
+    mv output ${LOGFOLDER}/0
+
+    echo "Run initial completed" >> $LOGFILE
+    date -u >> $LOGFILE
+
+    change_servers & 
+    wait $! 
+fi 
