62dbe3bddff19862c98657c8433a1b0bca1646b4
ZiluTian <zilu.tian@epfl.ch>
2019-06-03 07:05:58 -0400

    Update scheduler with locking

--- web-search/schedule.sh
+++ web-search/schedule.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 LOGFILE=log 
-LOGFOLDER=cavium/experimental2/
+LOGFOLDER=cavium/experimental6/
 
 echo "Run started" > $LOGFILE 
 date -u >> $LOGFILE
@@ -19,25 +19,38 @@ MOD=( 's/CLIENT_CPUS=.*$/CLIENT_CPUS=0-1/g;s/SERVER_CPUS=.*$/SERVER_CPUS=28-29/g
 's/CLIENT_CPUS=.*$/CLIENT_CPUS=0-13/g;s/SERVER_CPUS=.*$/SERVER_CPUS=28-41/g' \
 's/CLIENT_CPUS=.*$/CLIENT_CPUS=0-27/g;s/SERVER_CPUS=.*$/SERVER_CPUS=28-55/g' 
 )
+
+sed -ri s/^START_SERVER.*$/START_SERVER=false/g benchmark_run.sh
+
+MOD=( 's/SERVER_CPUS=.*$/SERVER_CPUS=112,140/g' \
+'s/SERVER_CPUS=.*$/SERVER_CPUS=112,140,168/g' \
+'s/SERVER_CPUS=.*$/SERVER_CPUS=112,140,168,196/g' \
+'s/SERVER_CPUS=.*$/SERVER_CPUS=112-113,140-141,168-169,196-197/g' \
+'s/SERVER_CPUS=.*$/SERVER_CPUS=112-115,140-143,168-171,196-199/g' \
+'s/SERVER_CPUS=.*$/SERVER_CPUS=112-125,140-153,168-181,196-209/g' \
+'s/SERVER_CPUS=.*$/SERVER_CPUS=112-223/g' 
+)
 fi 
 
-sed -ri s/START_SERVER.*$/START_SERVER=false/g benchmark_run.sh
-
-MOD=( 's/SERVER_CPUS=.*$/SERVER_CPUS=28-29/g' \
-'s/SERVER_CPUS=.*$/SERVER_CPUS=28-30/g' \
-'s/SERVER_CPUS=.*$/SERVER_CPUS=28-31/g' \
-'s/SERVER_CPUS=.*$/SERVER_CPUS=28-32/g' \
-'s/SERVER_CPUS=.*$/SERVER_CPUS=28-41/g' \
-'s/SERVER_CPUS=.*$/SERVER_CPUS=28-55/g'
+MOD=( 's/^CLIENT_CPUS=.*$/CLIENT_CPUS=0,1/g' \
+'s/^CLIENT_CPUS=.*$/CLIENT_CPUS=0,1,2/g' \
+'s/^CLIENT_CPUS=.*$/CLIENT_CPUS=0,1,2,3/g' \
+'s/^CLIENT_CPUS=.*$/CLIENT_CPUS=0,1,2,3,4/g' \
+'s/^CLIENT_CPUS=.*$/CLIENT_CPUS=0,1,2,3,4,5/g' \
+'s/^CLIENT_CPUS=.*$/CLIENT_CPUS=0,1,2,3,4,5,6/g' 
 )
-
 CNT=0
 MAX=${#MOD[@]}
+lockdir=/tmp/ws_scheduler.lock 
 
 while [[ ${CNT} -le ${MAX} ]]; do
+	# echo "Run ${CNT} started" >> $LOGFILE
+		
+	if mkdir $lockdir
+	then 
 	echo "Run ${CNT} started" >> $LOGFILE
-	
 	FOO=MOD[${CNT}]
+	
 	sed -ri ${!FOO} cpu_assign.sh
 	./benchmark_run.sh load.txt
 
@@ -46,5 +59,7 @@ while [[ ${CNT} -le ${MAX} ]]; do
 	
 	CNT=$((CNT + 1))
 	mv output ${LOGFOLDER}/${CNT}
+	rm $lockdir
+	fi 
 done
 
