ce836c5ed44c103b48c729451cb2829d9bd193cb
ZiluTian <zilu.tian@epfl.ch>
2019-06-08 18:20:29 +0200

    Include graph analytics

---
+++ graph-analytics/run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash 
+
+# set -x
+
+# trap 'kill ${dataset_ready} ${master_ready} ${worker_ready} ${rampup_ready} ${measurement_ready}; exit' SIGINT
+
+source safeguard
+source main_func
+
+(($DEV)) && echo $NUM_WORKERS
+
+create_dataset  
+create_network 
+
+start_master
+start_workers
+detect_stage master-ready 
+detect_stage workers-ready  
+
+start_client 
+detect_stage ramp-up
+echo "Rampup completed"
+perf stat -e $PERF_EVENTS --cpu $WORKER_CPUS_STR -p $WORKER_PIDS sleep infinity 2>>$PERF_LOG &
+detect_stage finished 
+echo "Finished"
+sudo pkill -fx "sleep infinity"
+
+cp user.cfg $OUT/user.cfg 
+docker logs $CLIENT_CONTAINER > $CLIENT_LOG
+log_folder
