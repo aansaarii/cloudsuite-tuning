3f6a454d908af375301432a2d16e10d056f818de
ZiluTian <zilu.tian@epfl.ch>
2019-06-08 17:38:12 +0200

    Factor out utils and perf events

--- in-memory-analytics/benchmark_run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-
-docker create --name data cloudsuite/movielens-dataset 
-docker network create spark-net 
-docker run -dP --net spark-net --hostname spark-master --name spark-master --cpuset-cpus=0 zilutian/spark master
-docker run -dP --net spark-net --volumes-from data --name spark-worker-01 --cpuset-cpus=1 zilutian/spark worker spark://spark-master:7077
