30e77a5db07da4066b7c2c6a72c6a91708b0691b
ZiluTian <zilu.tian@epfl.ch>
2019-06-10 20:37:23 +0200

    Add rps as an ar

--- data-caching/files/docker-entrypoint.sh
+++ data-caching/files/docker-entrypoint.sh
@@ -3,8 +3,9 @@
 # dataset scale: $1
 # worker number: $1 
 
-echo "args are $1 $2"
-
+# echo "args are $1 $2 $3"
+# $1: dataset scale factor, $2: number of workers, $3: target rps 
+ 
 /usr/src/memcached/memcached_client/loader \
     -a /usr/src/memcached/twitter_dataset/twitter_dataset_unscaled \
     -o /usr/src/memcached/twitter_dataset/twitter_dataset_{$1}x \
@@ -14,5 +15,5 @@ echo "args are $1 $2"
 /usr/src/memcached/memcached_client/loader \
     -a /usr/src/memcached/twitter_dataset/twitter_dataset_{$1}x \
     -s /usr/src/memcached/memcached_client/servers.txt \
-    -g 0.8 -c 200 -w $2 -e -r 1800 -T 1
+    -g 0.8 -c 200 -w $2 -e -r $3 -T 1
 
