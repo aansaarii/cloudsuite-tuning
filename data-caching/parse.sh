217d46f0c23cc883a21650d46a31e642512e02d6
Zilu Tian <zilu.tian@epfl.ch>
2019-06-12 23:35:45 +0200

    Clean up parsing scripts

--- data-caching/parse.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-
-# 95th latency 
-cat out/client-result.txt | grep -A1 95th | grep -v 95th | awk '{print $10}'| sed '/^$/d'
-
-# std 
-cat out/client-result.txt | grep -A1 95th | grep -v 95th | awk '{print $12}'| sed '/^$/d'
