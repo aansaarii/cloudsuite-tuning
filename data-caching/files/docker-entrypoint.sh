620640ce2fc1af172ab452b7a59f315f60cde6e1
Zilu Tian <zilu.tian@epfl.ch>
2019-06-12 23:43:19 +0200

    Seg error

--- data-caching/files/docker-entrypoint.sh
+++ data-caching/files/docker-entrypoint.sh
@@ -1,19 +1,19 @@
 #!/bin/bash 
-
+set -x
 # dataset scale: $1
 # worker number: $1 
 
-# echo "args are $1 $2 $3"
+echo "args are $1 $2 $3"
 # $1: dataset scale factor, $2: number of workers, $3: target rps 
  
 /usr/src/memcached/memcached_client/loader \
     -a /usr/src/memcached/twitter_dataset/twitter_dataset_unscaled \
     -o /usr/src/memcached/twitter_dataset/twitter_dataset_{$1}x \
     -s /usr/src/memcached/memcached_client/servers.txt \
-    -w $2 -S $2 -D 2048 -j
+    -w "$2" -S "$1" -D 2048 -j
 
 /usr/src/memcached/memcached_client/loader \
     -a /usr/src/memcached/twitter_dataset/twitter_dataset_{$1}x \
     -s /usr/src/memcached/memcached_client/servers.txt \
-    -g 0.8 -c 200 -w $2 -e -r $3 -T 1
+    -g 0.8 -c 200 -w "$2" -e -r "$3" -T 1
 
