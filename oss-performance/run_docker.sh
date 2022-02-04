ae6fc70b1b10917165232de2f870e12a933e8061
xusine <xusine@outlook.com>
2022-02-04 10:27:54 +0100

    Add CPU affinity

--- oss-performance/run_docker.sh
+++ oss-performance/run_docker.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-# 0. figure out the ip address
+# 0. figure out the ip address and core affinity.
+LOCAL_IP=$(ip route get 1.0.0.0 | grep src | cut -d' ' -f7)
+
+DB_CPUS=0,2,4,6
+HHVM_CPUS=8,10,12,14
+SIEGE_CPUS=1,3,5,7,9,11,13,15
 
 # 1. kill existing docker
 docker kill siege
 docker kill db_server
 
 # 2. run the db_server docker.
-docker run -dt --rm --name=db_server --net=host cloudsuite/mysql:mariadb-10.3
+docker run -dt --rm --name=db_server --cpuset-cpus=$DB_CPUS --net=host cloudsuite/mysql:mariadb-10.3
 
 # 3. run the siege client
-docker run --name=siege --rm -dt --net=host cloudsuite/siege:4.0.3rc3 $LOCAL_IP $LOCAL_IP
+docker run --name=siege --cpuset-cpus=$SIEGE_CPUS --rm -dt --net=host cloudsuite/siege:4.0.3rc3 $LOCAL_IP $LOCAL_IP
 
 sleep 5
 
@@ -20,8 +25,8 @@ sleep 5
 echo "" > result.json
 
 # 4.2 setup docker
-docker run --net=host --name=fb --rm \
-  -v $(pwd)/cmd.batch.sh:/oss-performance/cmd.sh \
+docker run --net=host --name=fb --rm --cpuset-cpus=$HHVM_CPUS \
+  -v $(pwd)/cmd.sh:/oss-performance/cmd.sh \
   -v $(pwd)/batch.conf.json:/oss-performance/batch.conf.json \
   -v $(pwd)/result.json:/oss-performance/result.json \
   cloudsuite/fb-oss-performance:4.0
