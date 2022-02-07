aa4a777dad9b549a2e689f8b58ed4952f583f6ce
xusine <xusine@outlook.com>
2022-02-07 17:39:13 +0100

    Add client_thread generator

--- oss-performance/gen_batch.py
+++ oss-performance/gen_batch.py
@@ -3,15 +3,6 @@
 import json
 
 base_structure = {
-  "runtimes": {
-    "Name": {
-      "type": "hhvm",
-      "bin": "/usr/local/bin/hhvm",
-      "args": [
-        "--client-threads=20"
-      ]
-    },
-  },
   "targets": [
     "wordpress" 
   ],
@@ -22,13 +13,47 @@ base_structure = {
     "password": "root",
     "options": [
       "cpu-fraction=0.5",
-      "i-am-not-benchmarking"
+      "i-am-not-benchmarking",
+      "server-threads=4"
     ]
-  }
+  },
+  "runtimes": {
+    
+  },
 }
 
 import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument()
+parser.add_argument("-r", nargs=3, type=int, help="Range. Using Python's range to generate number of client threads.")
+parser.add_argument("-f", type=argparse.FileType("r"), help="File contains the number of client threads.")
+parser.add_argument("output", help="Generated JSON file for batching run HHVM.", type=argparse.FileType("w"))
+
+args = parser.parse_args()
+
+# 1. checking arguments
+
+if args.r is None and args.f is None:
+  print("You should either use -r or -f to provide thread numbers.")
+  exit(0)
+
+client_numbers= []
+
+if args.r is None:
+  for l in args.f:
+    client_numbers.append(int(l))
+else:
+  client_numbers = range(*args.r)
+
+# generate the json
+for th in client_numbers:
+  base_structure["runtimes"]["{}C".format(th)] = {
+    "type": "hhvm",
+    "bin": "/usr/local/bin/hhvm",
+    "args": [
+      "--client-threads={}".format(th)
+    ]
+  }
+
+json.dump(base_structure, args.output, indent=4)
