2202f841444b065709a5b041bd920186ae2fbac0
Shanqing Lin <sqlin@n125.parsacluster.epfl.ch>
2022-03-18 10:28:56 +0100

    Change server threads number is available

--- oss-performance/gen_batch.py
+++ oss-performance/gen_batch.py
@@ -12,9 +12,7 @@ base_structure = {
     "username": "root",
     "password": "root",
     "options": [
-      "cpu-fraction=0.5",
       "i-am-not-benchmarking",
-      "server-threads=4"
     ]
   },
   "runtimes": {
@@ -26,34 +24,27 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-r", nargs=3, type=int, help="Range. Using Python's range to generate number of client threads.")
-parser.add_argument("-f", type=argparse.FileType("r"), help="File contains the number of client threads.")
+parser.add_argument("-c", nargs=3, type=int, help="Using Python's range to generate number of concurrent clients.")
+parser.add_argument("-s", nargs=3, type=int, help="Using Python's range to generate number of server threads.")
 parser.add_argument("output", help="Generated JSON file for batching run HHVM.", type=argparse.FileType("w"))
 
 args = parser.parse_args()
 
 # 1. checking arguments
 
-if args.r is None and args.f is None:
-  print("You should either use -r or -f to provide thread numbers.")
-  exit(0)
-
-client_numbers= []
-
-if args.r is None:
-  for l in args.f:
-    client_numbers.append(int(l))
-else:
-  client_numbers = range(*args.r)
+client_numbers = range(*args.c)
+server_threads = range(*args.s)
 
 # generate the json
-for th in client_numbers:
-  base_structure["runtimes"]["{}C".format(th)] = {
-    "type": "hhvm",
-    "bin": "/usr/local/bin/hhvm",
-    "args": [
-      "--client-threads={}".format(th)
-    ]
-  }
+for sth in server_threads:
+    for th in client_numbers:
+        base_structure["runtimes"]["{}S{}C".format(sth,th)] = {
+            "type": "hhvm",
+            "bin": "/usr/local/bin/hhvm",
+            "args": [
+                "--client-threads={}".format(th),
+                "--server-threads={}".format(sth)
+            ]
+        }
 
 json.dump(base_structure, args.output, indent=4)
